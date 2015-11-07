675d64a1f78910113c5d165d5427452df0646b9b
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-11-06 16:15:11 -0800

    making update function

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -2,11 +2,41 @@
  * Created by yasudayousuke on 11/2/15.
  */
 $(function(){
-    $(document).on('click','input#message_form_submit_input', function(e){
+    var editorStorageName = "editorStorage";
+    localStorage[editorStorageName] = JSON.stringify([])
+    if(!localStorage[editorStorageName]){
+        localStorage[editorStorageName] = JSON.stringify([]);
+    }else{
+        JSON.parse(localStorage[editorStorageName]).forEach(function(editorJson, index){
+            addDomToArticleList(editorJson, index);
+        });
+    }
+
+    var articleEditorListArray = JSON.parse(localStorage[editorStorageName]);
+    var currentListNumber;
+
+    $(document).on('click','input#blog_form_submit_input', function(e){
         e.preventDefault();
-        var trix_element = document.querySelector("trix-editor");
-        var trix_editor_dom = trix_element.editor.getDocument();
-        document.querySelector("#message_box").innerHTML = trix_element.innerHTML;
-        console.log(trix_editor_dom);
-    })
+        var trix_element = document.querySelector("trix-editor#blog_editor");
+        articleEditorListArray.push(trix_element.editor);
+        localStorage[editorStorageName] = JSON.stringify(articleEditorListArray);
+        addDomToArticleList(JSON.parse(JSON.stringify(trix_element.editor)));
+    });
+
+    $(document).on('click', 'li.trix-blog', function(){
+        var this_list_number = this.getAttribute('list-id');
+        if(this_list_number != currentListNumber){
+            this.innerHTML = this.innerHTML + '<div class="torix_editor"><form><trix-editor></trix-editor><input type="submit" value="update"/></form></div>';
+            currentListNumber = this_list_number;
+        }
+    });
 });
+
+function addDomToArticleList(editorJson, list_id){
+    var article_ul_dom = document.querySelector("#article_ul");
+    var trix_parser_dom = document.querySelector("trix-editor#trix_parser");
+    trix_parser_dom.editor.loadJSON(editorJson);
+    var editorHtml = trix_parser_dom.innerHTML;
+    var li_html = '<li class="trix-blog" list-id="' + list_id + '"><div class="trix-content">'+ editorHtml +'</div></li><hr/>';
+    article_ul_dom.innerHTML += li_html;
+}
